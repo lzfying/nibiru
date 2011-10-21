c12279aa497183f3a461f0b17c8ad505020d1a2b
lbrasseur <lbrasseur@gmail.com>
2011-10-21 16:01:42 -0300

    nibiru sample - first commit

---
+++ ar.com.oxen.nibiru.sample/ar.com.oxen.nibiru.sample.module/src/main/java/ar/com/oxen/nibiru/sample/domain/Course.java
@@ -0,0 +1,77 @@
+package ar.com.oxen.nibiru.sample.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import ar.com.oxen.nibiru.crud.bean.annotation.Action;
+import ar.com.oxen.nibiru.crud.bean.annotation.Actions;
+import ar.com.oxen.nibiru.crud.bean.annotation.Show;
+import ar.com.oxen.nibiru.crud.bean.annotation.Widget;
+import ar.com.oxen.nibiru.crud.manager.api.CrudAction;
+import ar.com.oxen.nibiru.crud.manager.api.WidgetType;
+
+@Entity
+@Actions({
+		@Action(name = CrudAction.NEW, requiresEntity = false, showInForm = false),
+		@Action(name = CrudAction.EDIT, requiresEntity = true, showInForm = false),
+		@Action(name = CrudAction.UPDATE, requiresEntity = true, showInList = false),
+		@Action(name = CrudAction.DELETE, requiresEntity = true, showInForm = false, requiresConfirmation = true), })
+public class Course {
+	@Id
+	@GeneratedValue
+	@Show(order = 0)
+	@Widget(type = WidgetType.TEXT_FIELD, readonly = true)
+	private Integer id;
+
+	@Column
+	@Show(order = 10)
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
---
+++ ar.com.oxen.nibiru.sample/ar.com.oxen.nibiru.sample.module/src/main/java/ar/com/oxen/nibiru/sample/module/ModuleConfigurator.java
@@ -0,0 +1,69 @@
+package ar.com.oxen.nibiru.sample.module;
+
+import ar.com.oxen.nibiru.crud.manager.api.CrudActionExtension;
+import ar.com.oxen.nibiru.crud.manager.api.CrudManager;
+import ar.com.oxen.nibiru.crud.utils.AbstractCrudModuleConfigurator;
+import ar.com.oxen.nibiru.sample.domain.Course;
+import ar.com.oxen.nibiru.sample.domain.Student;
+import ar.com.oxen.nibiru.sample.domain.Subject;
+import ar.com.oxen.nibiru.ui.api.extension.SubMenuExtension;
+import ar.com.oxen.nibiru.ui.utils.extension.SimpleSubMenuExtension;
+
+public class ModuleConfigurator extends AbstractCrudModuleConfigurator {
+	private static final String MENU_EXTENSION = "ar.com.oxen.nibiru.menu.sample.crud";
+
+	private CrudManager<Subject> subjectCrudManager;
+	private CrudActionExtension<Subject> subjectCrudActionExtension;
+
+	private CrudManager<Course> courseCrudManager;
+	private CrudActionExtension<Course> courseCrudActionExtension;
+
+	private CrudManager<Student> studentCrudManager;
+	private CrudActionExtension<Student> studentCrudActionExtension;
+
+	@Override
+	protected void configure() {
+		this.registerExtension(new SimpleSubMenuExtension("sample.crud",
+				MENU_EXTENSION, 1), "ar.com.oxen.nibiru.menu",
+				SubMenuExtension.class);
+
+		this.addCrudWithMenu("sample.crud.subject", MENU_EXTENSION,
+				this.subjectCrudManager, this.subjectCrudActionExtension);
+
+		// this.addChildCrudWithMenu("editarVideos", this.libroCrudManager,
+		// "libro", this.videoCrudManager, this.videoCrudActionExtension);
+
+		this.addCrudWithMenu("sample.crud.course", MENU_EXTENSION,
+				this.courseCrudManager, this.courseCrudActionExtension);
+
+		this.addCrudWithMenu("sample.crud.student", MENU_EXTENSION,
+				this.studentCrudManager, this.studentCrudActionExtension);
+	}
+
+	public void setSubjectCrudManager(CrudManager<Subject> subjectCrudManager) {
+		this.subjectCrudManager = subjectCrudManager;
+	}
+
+	public void setSubjectCrudActionExtension(
+			CrudActionExtension<Subject> subjectCrudActionExtension) {
+		this.subjectCrudActionExtension = subjectCrudActionExtension;
+	}
+
+	public void setCourseCrudManager(CrudManager<Course> courseCrudManager) {
+		this.courseCrudManager = courseCrudManager;
+	}
+
+	public void setCourseCrudActionExtension(
+			CrudActionExtension<Course> courseCrudActionExtension) {
+		this.courseCrudActionExtension = courseCrudActionExtension;
+	}
+
+	public void setStudentCrudManager(CrudManager<Student> studentCrudManager) {
+		this.studentCrudManager = studentCrudManager;
+	}
+
+	public void setStudentCrudActionExtension(
+			CrudActionExtension<Student> studentCrudActionExtension) {
+		this.studentCrudActionExtension = studentCrudActionExtension;
+	}
+}
