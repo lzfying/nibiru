02032a22ca9ef597008a86afb56672340381807f
lbrasseur <lbrasseur@gmail.com>
2012-04-25 00:11:39 -0300

    service and implementation separation (different bundles) and crud presenter/view separation

---
+++ ar.com.oxen.nibiru.crud/ar.com.oxen.nibiru.crud.ui.generic.presenter/src/main/java/ar/com/oxen/nibiru/crud/ui/generic/presenter/GenericCrudPresenterFactory.java
@@ -0,0 +1,58 @@
+package ar.com.oxen.nibiru.crud.ui.generic.presenter;
+
+import ar.com.oxen.commons.eventbus.api.EventBus;
+import ar.com.oxen.nibiru.conversation.api.ConversationFactory;
+import ar.com.oxen.nibiru.crud.manager.api.CrudManager;
+import ar.com.oxen.nibiru.crud.manager.api.EditCrudEntityEvent;
+import ar.com.oxen.nibiru.crud.ui.api.CrudPresenterFactory;
+import ar.com.oxen.nibiru.crud.ui.api.form.CrudFormView;
+import ar.com.oxen.nibiru.crud.ui.api.list.CrudListView;
+import ar.com.oxen.nibiru.crud.ui.generic.presenter.form.GenericCrudFormPresenter;
+import ar.com.oxen.nibiru.crud.ui.generic.presenter.list.GenericCrudListPresenter;
+import ar.com.oxen.nibiru.crud.ui.generic.presenter.list.GenericCrudListPresenterByParent;
+import ar.com.oxen.nibiru.extensionpoint.api.ExtensionPointManager;
+import ar.com.oxen.nibiru.ui.api.mvp.Presenter;
+
+public class GenericCrudPresenterFactory implements CrudPresenterFactory {
+	private EventBus eventBus;
+	private ExtensionPointManager extensionPointManager;
+	private ConversationFactory conversationFactory;
+
+	@Override
+	public Presenter<CrudListView> buildListPresenter(
+			CrudManager<?> crudManager) {
+		return new GenericCrudListPresenter(crudManager, this.eventBus, 
+				this.conversationFactory.buildConversation(),
+				this.extensionPointManager);
+	}
+
+	@Override
+	public Presenter<CrudListView> buildListPresenter(
+			CrudManager<?> crudManager, String parentField, Object parentValue) {
+		return new GenericCrudListPresenterByParent(crudManager, this.eventBus,
+				this.conversationFactory.buildConversation(),
+				this.extensionPointManager, parentField, parentValue);
+	}
+
+	@Override
+	public Presenter<CrudFormView> buildFormPresenter(
+			CrudManager<?> crudManager, EditCrudEntityEvent event) {
+		return new GenericCrudFormPresenter(crudManager, this.eventBus, 
+				event.getConversation(), event.getCrudEntity(),
+				this.extensionPointManager);
+	}
+
+	public void setEventBus(EventBus eventBus) {
+		this.eventBus = eventBus;
+	}
+
+	public void setExtensionPointManager(
+			ExtensionPointManager extensionPointManager) {
+		this.extensionPointManager = extensionPointManager;
+	}
+
+	public void setConversationFactory(ConversationFactory conversationFactory) {
+		this.conversationFactory = conversationFactory;
+	}
+
+}
---
+++ ar.com.oxen.nibiru.crud/ar.com.oxen.nibiru.crud.ui.generic.presenter/src/main/java/ar/com/oxen/nibiru/crud/ui/generic/presenter/list/GenericCrudListPresenter.java
@@ -0,0 +1,28 @@
+package ar.com.oxen.nibiru.crud.ui.generic.presenter.list;
+
+import java.util.List;
+
+import ar.com.oxen.commons.eventbus.api.EventBus;
+import ar.com.oxen.nibiru.conversation.api.Conversation;
+import ar.com.oxen.nibiru.crud.manager.api.CrudEntity;
+import ar.com.oxen.nibiru.crud.manager.api.CrudManager;
+import ar.com.oxen.nibiru.extensionpoint.api.ExtensionPointManager;
+
+public class GenericCrudListPresenter extends AbstractGenericCrudListPresenter {
+
+	public GenericCrudListPresenter(CrudManager<?> crudManager,
+			EventBus eventBus, Conversation conversation,
+			ExtensionPointManager extensionPointManager) {
+		super(crudManager, eventBus, conversation, extensionPointManager);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected <K> List<CrudEntity<K>> findEntities() {
+		return ((CrudManager<K>) this.getCrudManager()).findAll();
+	}
+
+	@Override
+	protected <K> void onReturnedEntity(CrudEntity<K> returnedEntity) {
+	}
+}
