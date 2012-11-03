64e2ec9fc8ec12bb6f0a524323d2ad7292eafc30
lbrasseur <lbrasseur@gmail.com>
2012-11-03 17:30:23 -0300

    Security RPC integration with profile

--- main/ar.com.oxen.nibiru.security/ar.com.oxen.nibiru.security.rpc/src/main/java/ar/com/oxen/nibiru/security/rpc/impl/RpcAuthenticationServiceImpl.java
+++ main/ar.com.oxen.nibiru.security/ar.com.oxen.nibiru.security.rpc/src/main/java/ar/com/oxen/nibiru/security/rpc/impl/RpcAuthenticationServiceImpl.java
@@ -5,16 +5,19 @@ import ar.com.oxen.nibiru.security.rpc.RpcAuthenticationService;
 import ar.com.oxen.nibiru.security.rpc.UserDto;
 import ar.com.oxen.nibiru.security.api.AuthenticationService;
 import ar.com.oxen.nibiru.security.api.BadCredentialsException;
+import ar.com.oxen.nibiru.security.api.Profile;
 
 public class RpcAuthenticationServiceImpl implements RpcAuthenticationService {
 	private AuthenticationService authenticationService;
+	private Profile profile;
 
 	@Override
 	public UserDto login(LoginDto loginDto) {
 		try {
 			this.authenticationService.login(loginDto.getUsername(),
 					loginDto.getPassword());
-			return new UserDto("Pepe", "Sanchez");
+			return new UserDto(this.profile.getFirstName(),
+					this.profile.getLastName());
 		} catch (BadCredentialsException e) {
 			return null;
 		}
@@ -23,5 +26,9 @@ public class RpcAuthenticationServiceImpl implements RpcAuthenticationService {
 	public void setAuthenticationService(
 			AuthenticationService authenticationService) {
 		this.authenticationService = authenticationService;
+	}
+
+	public void setProfile(Profile profile) {
+		this.profile = profile;
 	}
 }
