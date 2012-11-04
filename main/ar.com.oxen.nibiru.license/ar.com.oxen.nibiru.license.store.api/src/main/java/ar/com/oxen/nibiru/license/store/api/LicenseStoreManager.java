a2a0203757b07bb2ea2f8cbb14c818bf5cf67c3c
lbrasseur <lbrasseur@gmail.com>
2012-11-04 20:44:29 -0300

    english license documentation

--- main/ar.com.oxen.nibiru.license/ar.com.oxen.nibiru.license.store.api/src/main/java/ar/com/oxen/nibiru/license/store/api/LicenseStoreManager.java
+++ main/ar.com.oxen.nibiru.license/ar.com.oxen.nibiru.license.store.api/src/main/java/ar/com/oxen/nibiru/license/store/api/LicenseStoreManager.java
@@ -1,9 +1,27 @@
 package ar.com.oxen.nibiru.license.store.api;
 
+/**
+ * Manage for loading and saving licenses.
+ */
 public interface LicenseStoreManager {
 	String GENERIC_MODULE = "genericModule";
 
+	/**
+	 * Loads a license for a given module.
+	 * 
+	 * @param module
+	 *            The module
+	 * @return A String representing the license
+	 */
 	String loadLicense(String module);
 
+	/**
+	 * Saves a license for a given module.
+	 * 
+	 * @param module
+	 *            The module
+	 * @param license
+	 *            A String representing the license
+	 */
 	void saveLicense(String module, String license);
 }
