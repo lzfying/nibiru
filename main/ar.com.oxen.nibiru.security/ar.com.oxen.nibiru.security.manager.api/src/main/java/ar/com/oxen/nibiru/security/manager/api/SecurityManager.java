cfe620017870bd21241c77c82e536aa30abe728a
lbrasseur <lbrasseur@gmail.com>
2012-11-14 16:39:25 -0300

    0.3 version documentation update

--- main/ar.com.oxen.nibiru.security/ar.com.oxen.nibiru.security.manager.api/src/main/java/ar/com/oxen/nibiru/security/manager/api/SecurityManager.java
+++ main/ar.com.oxen.nibiru.security/ar.com.oxen.nibiru.security.manager.api/src/main/java/ar/com/oxen/nibiru/security/manager/api/SecurityManager.java
@@ -1,8 +1,34 @@
 package ar.com.oxen.nibiru.security.manager.api;
 
+/**
+ * Security manager.
+ */
 public interface SecurityManager {
+	/**
+	 * Changes user's passowrd.
+	 * 
+	 * @param username
+	 *            The user name
+	 * @param oldPassword
+	 *            The previous password
+	 * @param newPassword
+	 *            The new password
+	 * @throws UserNotFoundException
+	 *             If no user with the given name is found
+	 * @throws InvalidOldPassword
+	 *             If previous password is not valid
+	 */
 	void changePassword(String username, String oldPassword, String newPassword)
 			throws UserNotFoundException, InvalidOldPassword;
 
+	/**
+	 * Retrieves the user data.
+	 * 
+	 * @param username
+	 *            The user name
+	 * @return The user data
+	 * @throws UserNotFoundException
+	 *             If no user with the given name is found
+	 */
 	UserData getUserData(String username) throws UserNotFoundException;
 }
