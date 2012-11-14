522b949f87665dae956d7c8e6736184cf05dd5b2
lbrasseur <lbrasseur@gmail.com>
2012-11-14 18:25:04 -0300

    documentation fixes

--- main/ar.com.oxen.nibiru.security/ar.com.oxen.nibiru.security.rpc/src/main/java/ar/com/oxen/nibiru/security/rpc/RpcAuthenticationService.java
+++ main/ar.com.oxen.nibiru.security/ar.com.oxen.nibiru.security.rpc/src/main/java/ar/com/oxen/nibiru/security/rpc/RpcAuthenticationService.java
@@ -1,5 +1,16 @@
 package ar.com.oxen.nibiru.security.rpc;
 
+/**
+ * Remote authentication service.
+ * 
+ */
 public interface RpcAuthenticationService {
+	/**
+	 * Performs a login.
+	 * 
+	 * @param loginDto
+	 *            A DTO with login request information
+	 * @return A DTO with user information
+	 */
 	UserDto login(LoginDto loginDto);
 }
