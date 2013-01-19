b1d0e459fb35ee3ddd8845f98cf9d86c45df8a11
lbrasseur <lbrasseur@gmail.com>
2013-01-19 20:20:03 -0300

    mail service migration

--- main/ar.com.oxen.nibiru.mail/ar.com.oxen.nibiru.mail.javamail/src/main/java/ar/com/oxen/nibiru/mail/javamail/JavaMailService.java
+++ main/ar.com.oxen.nibiru.mail/ar.com.oxen.nibiru.mail.javamail/src/main/java/ar/com/oxen/nibiru/mail/javamail/JavaMailService.java
@@ -18,6 +18,31 @@ import ar.com.oxen.nibiru.mail.api.MailService;
 
 public class JavaMailService implements MailService {
 	private Session session;
+	private String username;
+	private String password;
+	private String host;
+	private int port;
+	private boolean startTls;
+
+	public void init() {
+		final Properties mailProperties = new Properties();
+
+		if (username != null) {
+			mailProperties.put("mail.smtp.auth", "true");
+		}
+		if (startTls) {
+			mailProperties.put("mail.smtp.starttls.enable", "true");
+		}
+		mailProperties.put("mail.smtp.host", this.host);
+		mailProperties.put("mail.smtp.port", String.valueOf(this.port));
+
+		this.session = Session.getDefaultInstance(mailProperties,
+				new Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(username, password);
+					}
+				});
+	}
 
 	@Override
 	public void sendMail(MailMessage message) {
@@ -47,15 +72,23 @@ public class JavaMailService implements MailService {
 		}
 	}
 
-	public void setMailProperties(final Properties mailProperties) {
-		this.session = Session.getDefaultInstance(mailProperties,
-				new Authenticator() {
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(
-								(String) mailProperties.get("mail.smtp.user"),
-								(String) mailProperties
-										.get("mail.smtp.password"));
-					}
-				});
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public void setStartTls(boolean startTls) {
+		this.startTls = startTls;
 	}
 }
